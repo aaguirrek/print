a510f3e682a6dac5527fa48c85aee3bc581cc662
aaguirrek <aaguirre@ucsm.edu.pe>
2019-12-01 21:30:44 -0500

    ola

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,7 +14,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        
+        echo exec('git pull');
+        return;
         $empresa = Empresa::all();
         if( count( $empresa ) > 0 ){
               $empresa = $empresa[0];
